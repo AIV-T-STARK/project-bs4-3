28db4024f2b353084d11b4ac14d9933343317251
Nguyen Gia Hao <giahao9899@AIV.T.Shark>
2018-12-10 18:13:49 +0700

    v 1.0.0

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,31 @@ $(document).ready(function(){
         autoplayTimeout:4000,
         autoplayHoverPause:true
     });
+    $('#contact-member-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:4,
+                autoplay:false,
+                loop:false
+            }
+        }
+    });
+    $('[data-fancybox = "iframe"]').fancybox({
+        toolbar  : false,
+        smallBtn : true,
+        iframe : {
+            preload : false
+        }
+    });
 });
